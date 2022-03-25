88c14f700c30d0510d22deb3036c5e4a313507a1
Adam Outler <adamoutler@gmail.com>
2022-03-25 22:07:14 +0000

    Remove uart-wifi pip package from integration

--- src/uart_wifi/errors.py
+++
@@ -1,9 +0,0 @@
-"""Exceptions"""
-
-
-class AnycubicException(Exception):
-    """Base class for Anycubic exceptions."""
-
-
-class ConnectionException(AnycubicException):
-    """Problem when connecting"""
